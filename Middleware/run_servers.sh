eea706877d782c0600c566d8ac4ee49ff1760bc6
chellycheng <hehuimin.cheng@mail.mcgill.ca>
2020-10-04 00:17:26 -0400

    Fix the order bug

--- Middleware/run_servers.sh
+++ Middleware/run_servers.sh
@@ -9,10 +9,10 @@ tmux new-session \; \
 	split-window -v \; \
 	select-layout main-vertical \; \
 	select-pane -t 1 \; \
-	send-keys "sshpass -p $1 ssh -t ${MACHINES[0]} \"cd /home/2018/hcheng35/COMP512/Distributed_System/Middleware > /dev/null; echo -n 'Connected to '; FlightServer; ./run_flightserver.sh 1048\"" C-m \; \
+	send-keys "sshpass -p $1 ssh -t ${MACHINES[0]} \"cd /home/2018/hcheng35/COMP512/Distributed_System/Middleware > /dev/null; echo -n 'Connected to '; CarServer; ./run_carserver.sh 1048\"" C-m \; \
 	select-pane -t 2 \; \
-	send-keys "sshpass -p $1 ssh -t ${MACHINES[1]} \"cd /home/2018/hcheng35/COMP512/Distributed_System/Middleware > /dev/null; echo -n 'Connected to '; RoomServer; ./run_roomserver.sh 1048\"" C-m \; \
+	send-keys "sshpass -p $1 ssh -t ${MACHINES[1]} \"cd /home/2018/hcheng35/COMP512/Distributed_System/Middleware > /dev/null; echo -n 'Connected to '; FlightServer; ./run_flightserver.sh 1048\"" C-m \; \
 	select-pane -t 3 \; \
-	send-keys "sshpass -p $1 ssh -t ${MACHINES[2]} \"cd /home/2018/hcheng35/COMP512/Distributed_System/Middleware > /dev/null; echo -n 'Connected to '; CarServer; ./run_carserver.sh 1048\"" C-m \; \
+	send-keys "sshpass -p $1 ssh -t ${MACHINES[2]} \"cd /home/2018/hcheng35/COMP512/Distributed_System/Middleware > /dev/null; echo -n 'Connected to '; RoomServer; ./run_roomserver.sh 1048\"" C-m \; \
 	select-pane -t 0 \; \
 	send-keys "sshpass -p $1 ssh -t ${MACHINES[3]} \"cd /home/2018/hcheng35/COMP512/Distributed_System/Middleware > /dev/null; echo -n 'Connected to '; MiddlewareServer; sleep .5s; ./run_server.sh ${MACHINES[0]} ${MACHINES[1]} ${MACHINES[2]} 1048\"" C-m \;
