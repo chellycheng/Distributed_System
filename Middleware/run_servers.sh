aef5ba9c5a69b7b88b6eb535c6d2373a0a1b23fb
chellycheng <hehuimin.cheng@mail.mcgill.ca>
2020-10-03 21:49:05 -0400

    Testing runservers

--- Middleware/run_servers.sh
+++ Middleware/run_servers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-#TODO: SPECIFY THE HOSTNAMES OF 4 CS MACHINES (lab2-8, lab2-10, etc...)
-#MACHINES=(hcheng35@teach-vw2.cs.mcgill.ca, hcheng35@teach-vw1.cs.mcgill.ca, hcheng35@teach-vw3.cs.mcgill.ca, hcheng35@teach-vw4.cs.mcgill.ca)
-MACHINES=(hcheng35@mimi.cs.mcgill.ca, hcheng35@mimi.cs.mcgill.ca, hcheng35@mimi.cs.mcgill.ca, hcheng35@mimi.cs.mcgill.ca)
+#TODO: SPECIFY THE HOSTNAMES OF 4 CS MACHINES (lab2--8, lab2-10, etc...)
+MACHINES=(hcheng35@teach-vw1, hcheng35@teach-vw2, hcheng35@teach-vw3, hcheng35@teach-vw4)
+#MACHINES=(hcheng35@mimi.cs.mcgill.ca, hcheng35@mimi.cs.mcgill.ca, hcheng35@mimi.cs.mcgill.ca, hcheng35@mimi.cs.mcgill.ca)
 tmux new-session \; \
 	split-window -h \; \
 	split-window -v \; \
